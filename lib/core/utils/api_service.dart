1514b473015f7dc1cec2ba9fe454819429ec1769
Abdalah-eslam <abdallaheslam2001@gmail.com>
2024-08-08 06:10:48 +0300

    finish cubit and observerbloc and trager data

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -3,11 +3,11 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final _baseUrl = 'https://api.bigbookapi.com/search-books?api-key=$kapikey';
-  final Dio dio;
-  ApiService(this.dio);
+  final Dio _dio;
+  ApiService(this._dio);
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
-    var response = await dio.get('$_baseUrl$endpoint');
+    var response = await _dio.get('$_baseUrl$endpoint');
     return response.data;
   }
 }
